06440481e75c46e4109713974b13627fafa981a0
Robin <robinlee_6316@msn.com>
2014-04-17 17:29:13 +0800

    new empty mvc5 solution

---
+++ ImageUploader.Web/ImageUploader.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageUploader.Web.Startup))]
+namespace ImageUploader.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
